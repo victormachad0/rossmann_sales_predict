ffc0f0836db4a227a97f8a48905e63d557fa1365
Victor <vmachado.contato@gmail.com>
2022-12-24 17:52:11 -0300

    correcao no request

--- web_app/telegram_bot.py
+++ web_app/telegram_bot.py
@@ -27,7 +27,7 @@ def send_message(chat_id, text):
     url = 'https://api.telegram.org/bot{}/'.format(TOKEN)
     url = url + 'sendMessage?chat_id={}'.format(chat_id)
     
-    requests.post( url, json={'text': text} )
+    r = requests.post( url, json={'text': text} )
     print( 'Status Code {}'.format(r.status_code) )
     
     return None
