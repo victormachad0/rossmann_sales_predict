f5dbebf9169c096f5e81adcf41970619b64dab7f
Victor <vmachado.contato@gmail.com>
2023-01-15 20:40:31 -0300

    adicionado secret

--- web_app/telegram_bot.py
+++ web_app/telegram_bot.py
@@ -6,7 +6,7 @@ from dotenv import find_dotenv, load_dotenv
 from flask import Flask, request, Response
 
 # constants
-TOKEN = '5810358733:AAGYHQRZi1cZUgvxXZLpf6ld9dVj_6ZTFHI'
+#TOKEN = '5810358733:AAGYHQRZi1cZUgvxXZLpf6ld9dVj_6ZTFHI'
 
 # Info about bot
 #https://api.telegram.org/bot5810358733:AAGYHQRZi1cZUgvxXZLpf6ld9dVj_6ZTFHI/getMe
@@ -21,7 +21,7 @@ TOKEN = '5810358733:AAGYHQRZi1cZUgvxXZLpf6ld9dVj_6ZTFHI'
 #https://api.telegram.org/bot5810358733:AAGYHQRZi1cZUgvxXZLpf6ld9dVj_6ZTFHI/sendMessage?chat_id=5936467313&text=Hi, i'm great, tks!
 
 def send_message(chat_id, text):
-    url = 'https://api.telegram.org/bot{}/'.format(TOKEN)
+    url = 'https://api.telegram.org/bot{}/'.format(TELEGRAM_KEY)
     url = url + 'sendMessage?chat_id={}'.format(chat_id)
     
     r = requests.post( url, json={'text': text} )
