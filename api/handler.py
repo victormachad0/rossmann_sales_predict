134aa7031705a7641d5faa9268ea064598b1ebfe
Victor <vmachado.contato@gmail.com>
2023-05-18 20:02:35 -0300

    teste handler

--- api/handler.py
+++ api/handler.py
@@ -5,7 +5,7 @@ from flask import Flask, request, Response
 from rossmann.Rossmann import Rossmann
 
 # loading model
-model = pickle.load( open('model/model_rossmann.pkl', 'rb') )
+model = pickle.load( open('api/model/model_rossmann.pkl', 'rb') )
 
 # Initialize API
 app = Flask(__name__)
